207484086128c7da203d9723cd817c1db6a0dbdb
coding-quokka101 <dbsrud39840@gmail.com>
2026-03-12 00:19:25 +0900

    fix/기간별 분산 컨트롤러 통합 및 Swagger 설명 추가

--- src/main/java/com/uplus/crm/domain/analysis/dto/KeywordRankingResponse.java
+++ src/main/java/com/uplus/crm/domain/analysis/dto/KeywordRankingResponse.java
@@ -14,6 +14,7 @@ import org.bson.Document;
 @NoArgsConstructor
 @AllArgsConstructor
 @Builder
+@com.fasterxml.jackson.annotation.JsonInclude(com.fasterxml.jackson.annotation.JsonInclude.Include.NON_NULL)
 public class KeywordRankingResponse {
 
     private String date;
